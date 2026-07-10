6170b58df74353090b7e8d679cf6569f2f785ab0
Jakub Recman <jakub@oxidian.com>
2026-07-10 08:21:14 +0200

    Use dev runner for hpc

--- generate-workflows.py
+++ generate-workflows.py
@@ -474,6 +474,7 @@ class Workflow:
                         "build_config": "${{ matrix.config_path }}",
                         "dependencies": "\n".join(cmake_deps),
                         "python_dependencies": "\n".join(python_deps),
+                        "dev_runner": True,
                     },
                 }
                 if pkg_conf.get("requirements_path"):
